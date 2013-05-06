d6d49a034fe42ccff8487fcd1122da34245394ed
johndavid400 <jd@isotope11.com>
2013-05-06 16:49:35 -0500

    added test csv file and updated readme

--- README.md
+++ README.md
@@ -5,6 +5,14 @@ Simple Importer
 
 ## This importer uses the CSV library to read a csv file and import each row for use in your app.
 
-#### To begin, select a file to import, we will use the example file provided. 
+#### To begin the importer, you will need to copy the importer.rb file to your local Rails app into the lib/ directory. 
 
+####If you would also like to test with my example csv file, copy the "people.csv" file into your lib directory also.
 
+### Open a terminal and type: 
+
+    require 'importer'
+    a = SimpleImporter.new("lib/people.csv")
+    a.handle_rows
+
+### That's it! you should see the output of the csv file printed to your terminal screen.
--- lib/importer.rb
+++ lib/importer.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-class Importer
+class SimpleImporter
   attr_accessor :filename
 
   def initialize(filename)
@@ -9,13 +9,17 @@ class Importer
 
   def handle_rows
     @file = CSV.read(@filename, :col_sep => ",")
+
     @file.each do |row|
       # Do something with the data from each column in this row
       first_column = row[0]
       second_column = row[1]
       third_column = row[2]
+      fourth_column = row[3]
       # Typically, you would create a new record in your database using the data from each row:
       # Post.create(title: first_column, body: second_column, submitted_by: third_column)
+      # we are just going to puts the output to the terminal for testing
+      puts "#{first_column} , #{second_column} , #{third_column} , #{fourth_column}"
     end
   end
 
